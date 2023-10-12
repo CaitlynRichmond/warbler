79c87cb8cab6b05b64586175a58b985010a38aab
Caitlyn Richmond <caitlyn.a.richmond@gmail.com>
2023-10-12 14:33:46 -0400

    Tests oops

--- test_user_model.py
+++ test_user_model.py
@@ -15,7 +15,7 @@ from models import db, User, Message, Follow
 # before we import our app, since that will have already
 # connected to the database
 
-os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
+os.environ["DATABASE_URL"] = "postgresql:///warbler_test"
 
 # Now we can import app
 
@@ -49,3 +49,46 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u1.messages), 0)
         self.assertEqual(len(u1.followers), 0)
+
+    def test_is_following_true(self):
+        """Test case where u1 is following u2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u1_id)
+        u1.following.append(u2)
+
+        self.assertEqual(u1.is_following(u2), True)
+        self.assertIn(u2, u1.following)
+
+    def test_is_following_false(self):
+        """Test case where u1 isn't following u2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u1_id)
+
+        self.assertEqual(u1.is_following(u2), False)
+        self.assertNotIn(u2, u1.following)
+
+    def test_is_followed_by_true(self):
+        """Test case where u1 is followed by u2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u1_id)
+        u1.followers.append(u2)
+
+        self.assertEqual(u1.is_followed_by(u2), True)
+        self.assertIn(u2, u1.followers)
+
+    def test_is_followed_by_false(self):
+        """Test case where u1 isn't followed by u2"""
+
+        u1 = User.query.get(self.u1_id)
+        u2 = User.query.get(self.u1_id)
+
+        self.assertEqual(u1.is_followed_by(u2), False)
+        self.assertNotIn(u2, u1.followers)
+
+    # def test_user_signup_bad(self):
+    #     """Tests several bad cases"""
+
+    #     u1 = User.signup("u1", "u1@email.com", "password", None)
